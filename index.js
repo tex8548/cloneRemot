3a4511eed7e0332296386c655589d6707ba055cb
Fernando Teixeira da Silva <teixeira.fts@edu.unifor.br>
2021-05-20 21:49:48 -0300

    commit 2

--- index.js
+++ index.js
@@ -3,37 +3,28 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function(req, res) {
-    res.send("<h1>Bem vindo ao meu site!</h1>");
-})
+            app.get("/consulta/:parametro", function(req, res) {
+                //req --> dados enviados pelo cliente
+                //res --> resposta enviada pelo servidor de volta ao cliente
+                res.send("retorno consulta:" + req.params.parametro);
+            })
 
-//rota do cadastro de produtos
-app.get("/produtos", function(req, res) {
-    res.send("<h1>Lista de Produtos!</h1>");
-})
+            //rota com parametro opcional
+            app.get("/cadastro/:nome?", function(req, res) {
+                //req --> dados enviados pelo cliente
+                var nome = req.params.nome;
+                if (nome) {
+                    res.send("<h1>produto " + nome + " criado!</h1>");
+                } else {
+                    res.send("produto criado!");
+                }
 
-//rota com parametro 
-app.get("/consulta/:parametro", function(req, res) {
-    //req --> dados enviados pelo cliente
-    //res --> resposta enviada pelo servidor de volta ao cliente
-    res.send("retorno consulta:" + req.params.parametro);
-})
+            })
 
-//rota com parametro opcional
-app.get("/cadastro/:nome?", function(req, res) {
-    //req --> dados enviados pelo cliente
-    var nome = req.params.nome;
-    if (nome) {
-        res.send("<h1>produto " + nome + " criado!</h1>");
-    } else {
-        res.send("produto criado!");
-    }
-
-})
-
-app.listen(process.env.PORT || 3000, function(erro) { // cria a aplicaÃ§Ã£o na porta 3000
-    if (erro) {
-        console.log("Erro ao Iniciar.");
-    } else {
-        console.log("Servidor Iniciado.");
-    }
-})
+            app.listen(process.env.PORT || 3000, function(erro) { // cria a aplicaÃ§Ã£o na porta 3000
+                if (erro) {
+                    console.log("Erro ao Iniciar.");
+                } else {
+                    console.log("Servidor Iniciado.");
+                }
+            })
